8bda0a6e7d53043b8324a2b405e6e8ce5464b2de
Chikimuras <alexandre.m.velia@gmail.com>
2022-08-10 09:50:33 +0200

    Modifying svg display

--- web/src/components/Logo/Logo.js
+++ web/src/components/Logo/Logo.js
@@ -1,11 +1,17 @@
-const Logo = (props) => {
+import {Component} from "react";
 
-  return (
-    <div className="logo-container opacity-20 hover:opacity-100 transition-opacity relative">
-      <img src={props.img} className="tech-logo" alt={props.alt} width='100'/>
-    </div>
-  )
+class Logo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {img: props.img, alt: props.alt};
+  }
 
+  render() {
+    return (
+      <div className="logo-container opacity-20 hover:opacity-100 transition-opacity relative">
+        <img src={this.state.img} className="tech-logo" alt={this.state.alt} width='100'/>
+      </div>
+    )
+  }
 }
-
 export default Logo
---
+++ web/src/svg.js
@@ -0,0 +1,4 @@
+const svg_logo = document.querySelectorAll('.tech-logo');
+svg_logo.forEach(logo => {
+  console.log(logo);
+})
